eeda2d800eea1f2fa73d839784100204bd073d5e
chenzhigang <979703986@qq.com>
2021-07-15 13:14:10 +0800

    subcode

--- king-of-competition/src/api/curl.js
+++ king-of-competition/src/api/curl.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import router from 'src/router'
 
 export const baseURL =
-    window.location.host.indexOf('192') > -1
+    window.location.host.indexOf('9003') > -1
     || window.location.host.indexOf('localhost') > -1
         ? '//chainstore.g2campaign.com/'
         : '/';
